494863892bf718bce42994ce2f403b36588c492d
igorskyflyer <igor.dvlpr@gmail.com>
2021-06-26 20:38:38 +0200

    Add uw() and ux() functions; refactor code

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const { platform } = require('os')
 // let's cache the pattern
 const pattern = /\/+/g
 const isWindows = platform() === 'win32'
-const slash = isWindows ? '\\' : '/'
+const winSlash = '\\'
+const unixSlash = '/'
+const slash = isWindows ? winSlash : unixSlash
+
+/**
+ * Transforms the given path.
+ * @private
+ * @param {string} pathSlash
+ * @param {string} fsPath
+ * @param {boolean} [addTrailingSlash=false]
+ * @returns {string}
+ */
+function transform(pathSlash, fsPath, addTrailingSlash = false) {
+  return fsPath.replace(pattern, pathSlash)
+}
 
 /**
  * Returns a proper file path for both UNIX-like
@@ -23,14 +37,40 @@ function upath(fsPath, addTrailingSlash = false) {
   }
 
   if (addTrailingSlash) {
-    fsPath += '/'
+    fsPath += unixSlash
   }
 
-  return fsPath.replace(pattern, slash)
+  return transform(slash, fsPath, addTrailingSlash)
+}
+
+/**
+ * Returns a proper file path for Windows operating system
+ * while ignoring the current host operating system.
+ * @see upath
+ * @param {string} fsPath
+ * @param {boolean} [addTrailingSlash=false]
+ * @returns {string}
+ */
+function uw(fsPath, addTrailingSlash = false) {
+  return transform(winSlash, fsPath, addTrailingSlash)
+}
+
+/**
+ * Returns a proper file path for UNIX-like operating systems
+ * while ignoring the current host operating system.
+ * @see upath
+ * @param {string} fsPath
+ * @param {boolean} [addTrailingSlash=false]
+ * @returns {string}
+ */
+function ux(fsPath, addTrailingSlash = false) {
+  return transform(unixSlash, fsPath, addTrailingSlash)
 }
 
 module.exports = {
   upath,
   u: upath,
+  uw,
+  ux,
   slash,
 }
